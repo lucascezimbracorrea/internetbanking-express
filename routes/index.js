6a7140eddafa3acdbbff3289067125569461cd8a
Lucas Cezimbra Corrêa <lucascezimbracorrea@gmail.com>
2020-06-17 02:25:32 -0300

    Adicionado repositório express adicionando saldo

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var accountBalance = 100;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', accountBalance: accountBalance });
+});
+
+/* GET add deposit. */
+router.get('/deposit', function(req, res) {
+  try {    
+    accountBalance += parseFloat(req.query.depositValue);
+    console.log(accountBalance);
+    
+    res.status(200).send({newAccountBallance: accountBalance, message: "Depósito realizado com sucesso"});
+  } catch (error) {
+    res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
+  }
+});
+
+module.exports = router;
