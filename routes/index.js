d51cda569121e68d0cac88cef69acf8ac7dd1f23
Gabriella Selbach <gabriellaselbach@gmail.com>
2020-06-17 16:38:37 -0300

    adicionado saque e deposito

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,96 @@
 var express = require('express');
 var router = express.Router();
-
+var count = 0;
 var accountBalance = 100;
-
+var Pessoas = [];
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express', accountBalance: accountBalance });
 });
 
 /* GET add deposit. */
-router.get('/deposit', function(req, res) {
-  try {    
+router.get('/deposit', function (req, res) {
+  try {
     accountBalance += parseFloat(req.query.depositValue);
     console.log(accountBalance);
-    
-    res.status(200).send({newAccountBallance: accountBalance, message: "Depósito realizado com sucesso"});
+
+    res.status(200).send({ newAccountBallance: accountBalance, message: "Depósito realizado com sucesso" });
   } catch (error) {
     res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
   }
 });
-router.get('/saldo', function(req, res) {
-  res.status(200).send({Ballance: accountBalance});
+router.get('/depositValor', function (req, res) {
+  try {
+    var pos = req.query.valor;
+    for (i = 0; i < Pessoas.length; i += 1) {
+      if (Pessoas[i].id == pos) {
+        Pessoas[i].saldo = parseFloat(Pessoas[i].saldo) + parseFloat(req.query.depositValue);
+      }
+    }
+
+    res.status(200).send({ pessoa: Pessoas, message: "Depósito realizado com sucesso" });
+  } catch (error) {
+    res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
+  }
 });
-router.get('/saque', function(req, res) {
-  try {    
+
+router.get('/saldo', function (req, res) {
+  res.status(200).send({ Ballance: accountBalance });
+});
+router.get('/saldonovo', function (req, res) {
+  var id = req.query.id;
+  for (i = 0; i < Pessoas.length; i += 1) {
+    if(Pessoas[i].id== parseInt(id))
+      res.status(200).send({ Ballance: Pessoas[i].saldo });
+  }
+});
+router.get('/cadPessoa', function (req, res) {
+  try {
+    count += 1;
+    p = new Object();
+    p.nome = req.query.nome;
+    p.CPF = req.query.CPF;
+    p.saldo = req.query.saldo;
+    p.id = count;
+    Pessoas.push(p);
+    res.status(200).send({ pessoa: p, message: "Pessoa Adicionada" });
+  } catch (error) {
+    res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
+  }
+});
+router.get('/verifyPessoa', function (req, res) {
+  try {
+    if (Array.isArray(Pessoas) && Pessoas.length) {
+      var ids = [];
+      for (i = 0; i < Pessoas.length; i += 1) {
+        ids.push(Pessoas[i].id);
+      }
+      res.status(200).send({ cod: 0, ids: ids });
+    }else
+      res.status(200).send({ cod: 1 });
+    }catch (error) {
+      res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
+    }
+});
+router.get('/saque', function (req, res) {
+  try {
     accountBalance -= parseFloat(req.query.SaqueValue);
     console.log(accountBalance);
-    
-    res.status(200).send({newAccountBallance: accountBalance, message: "Saque realizado com sucesso"});
+
+    res.status(200).send({ newAccountBallance: accountBalance, message: "Saque realizado com sucesso" });
+  } catch (error) {
+    res.status(400).send("Não foi possível realizar esse Saque. Por favor tente mais tarde");
+  }
+});
+router.get('/saqueValor', function (req, res) {
+  try {
+    var pos = req.query.valors;
+    for (i = 0; i < Pessoas.length; i += 1) {
+      if (Pessoas[i].id == pos) {
+        Pessoas[i].saldo = parseFloat(Pessoas[i].saldo) - parseFloat(req.query.SaqueValue);
+      }
+    }
+    res.status(200).send({ pessoa: Pessoas, message: "Saque realizado com sucesso" });
   } catch (error) {
     res.status(400).send("Não foi possível realizar esse Saque. Por favor tente mais tarde");
   }
