df4927988f66dc520af71536a7d5d06bfd2d4cf8
biscaglia <mateusbiscaglia@hotmail.com>
2020-06-17 12:44:27 -0300

    adicionado Mateus

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var accountBalance = 100;
+var accountBalance = 0;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,6 +17,21 @@ router.get('/deposit', function(req, res) {
     res.status(200).send({newAccountBallance: accountBalance, message: "Depósito realizado com sucesso"});
   } catch (error) {
     res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
+  }
+});
+
+/* GET add saque. */
+router.get('/sak', function(req, res) {
+  try {
+    if(accountBalance < req.query.depositValue){
+      res.status(400).send({newAccountBallance: accountBalance, message: "Valor insufuciente, tente um valor menor!"}); 
+    } else 
+      accountBalance -= parseFloat(req.query.depositValue);
+      console.log(accountBalance);
+
+      res.status(200).send({newAccountBallance: accountBalance, message: "Saque realizado com sucesso"});   
+  } catch (error) {
+    res.status(400).send({message: "Não foi possível realizar esse Saque. Por favor tente mais tarde"});
   }
 });
 
