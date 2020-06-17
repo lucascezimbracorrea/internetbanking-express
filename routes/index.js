8a25b16a0319d7be7d3af8db98b9fe6f68896af3
Fabricio Silva <fabricioferreira.rs@gmail.com>
2020-06-17 10:54:53 -0300

    Adicionando saque

--- routes/index.js
+++ routes/index.js
@@ -10,14 +10,33 @@ router.get('/', function(req, res, next) {
 
 /* GET add deposit. */
 router.get('/deposit', function(req, res) {
-  try {    
+  try {
     accountBalance += parseFloat(req.query.depositValue);
     console.log(accountBalance);
-    
+
     res.status(200).send({newAccountBallance: accountBalance, message: "Depósito realizado com sucesso"});
   } catch (error) {
     res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
   }
 });
+
+/* GET add saque. */
+router.get('/saque', function(req, res) {
+  try {
+    var vlSaque = req.query.saqueValue;
+    var message = ''
+    if (accountBalance < vlSaque){
+      message = 'Saldo Insificiente';
+    }else{
+      accountBalance -= parseFloat(vlSaque);
+      message = 'Saque realizado com sucesso. Seu novo saldo é de ' + accountBalance + '.';
+    }
+    console.log(accountBalance);
+    res.status(200).send({newAccountBallance: accountBalance, message: message});
+  } catch (error) {
+    res.status(400).send("Não foi possível realizar esse depósito. Por favor tente mais tarde");
+  }
+});
+
 
 module.exports = router;
